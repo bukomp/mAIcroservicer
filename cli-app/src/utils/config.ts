23e13aa40e4ebdff7b242453dda1a1d736f2a2c3
Edvard <edvard@shalaev.com>
2023-08-29 19:21:58 +0300

    feat: new skeleton built, now implementing functionality for proof of concept

--- cli-app/src/utils/config.ts
+++ cli-app/src/utils/config.ts
@@ -4,11 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const config = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
   PIPE: process.env.PIPE,
+  NODE_ENV: process.env.NODE_ENV,
   //DATABASE_URL: process.env.DATABASE_URL,
   //JWT_SECRET: process.env.JWT_SECRET,
 };
 
+console.log(config);
 export default config;
--- web-server/src/controllers/cli.controller.ts
+++ web-server/src/controllers/cli.controller.ts
@@ -1,9 +1,42 @@
 import express from 'express';
 const router = express.Router();
 
-router.post('/cli/test', (req, res) => {
-  // Handle the request here
-  res.send('CLI test request received');
+router.get('/cli/test', (req, res) => {
+  const { exec } = require('child_process');
+  exec(
+    "echo 'status' > my_pipe",
+    (error: { message: any }, stdout: any, stderr: any) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      res.send(`stdout: ${stdout}`);
+    }
+  );
+});
+
+router.get('/range', (req, res) => {
+  const min = req.query.min;
+  const max = req.query.max;
+  const number = req.query.number;
+
+  if (!min || !max || !number) {
+    res
+      .status(400)
+      .send({ error: 'Please provide min, max and number parameters' });
+    return;
+  }
+
+  if (number < min || number > max) {
+    res.status(400).send({ error: 'Number is out of the provided range' });
+    return;
+  }
+
+  res.send({ min, max, number });
 });
 
 export default router;
