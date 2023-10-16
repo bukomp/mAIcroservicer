4a93e2a490afa486631756aa1a22c74337e486cc
Edvard <edvard@shalaev.com>
2023-10-16 20:05:52 +0300

    feat: added posibility to give input prompt to the program by calling the app with -p flag specified

--- src/helpers/read_user_input.py
+++ src/helpers/read_user_input.py
@@ -1,3 +1,12 @@
-def read_user_input():
-  prompt = "Please enter your input: "
-  return input(prompt)
+import argparse
+
+
+def read_user_input() -> str:
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-p', '--prompt', help='User input prompt')
+  args = parser.parse_args()
+  if args.prompt:
+    return args.prompt
+  else:
+    prompt = "Please enter your input: "
+    return input(prompt)
