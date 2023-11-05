d0b033a0388b2028fa557ede23ad337b04f352af
Edvard <edvard@shalaev.com>
2023-11-05 21:05:28 +0300

    feat: refactored prompt dictionary functionality and versioning & updated dictionary script to be more modular

--- scripts/list_prompts_as_json.py
+++ scripts/list_prompts_as_json.py
@@ -8,28 +8,37 @@ base_dir = 'src/prompts'
 data = {}
 
 # Function to create a multilevel object based on folder structure
-def create_object(path):
-    obj = {}
-    for name in os.listdir(path):
-        if os.path.isfile(os.path.join(path, name)):
-            # Ignore .pyc and __pycache__ files
-            if name.endswith('.pyc') or name == '__pycache__' or name == 'README.md':
-                continue
-            # Extract the version from the filename
-            version = name.split('_v')[-1].replace('.txt', '')
-            # Create a dictionary entry for each file
-            obj[version] = os.path.join(path, name)
-        else:
-            if name.endswith('.pyc') or name == '__pycache__' or name == 'README.md':
-                continue
-            obj[name] = create_object(os.path.join(path, name))
-    return obj
+
+
+def create_object(path: str) -> dict:
+  obj: dict = {}
+  for name in os.listdir(path):
+    if os.path.isfile(os.path.join(path, name)):
+      # Ignore .pyc and __pycache__ files
+      if name.endswith('.pyc') or name == '__pycache__' or name == 'README.md':
+        continue
+      # Extract the name and version from the filename
+      file_name: str
+      version: str
+      file_name, version = name.split('_v')
+      version = version.replace('.txt', '')
+      # Create a dictionary entry for each file, grouped by name
+      if file_name not in obj:
+        obj[file_name] = {}
+      obj[file_name][version] = os.path.join(path, name).replace('\\', '/')
+    else:
+      if name.endswith('.pyc') or name == '__pycache__' or name == 'README.md':
+        continue
+      obj[name] = create_object(os.path.join(path, name))
+  return obj
+
 
 # Create the multilevel object
-data = create_object(base_dir)
+data: dict = create_object(base_dir)
 
 # Convert the dictionary to a JSON string
-json_data = json.dumps(data, indent=4)
+json_data: str = json.dumps(data, indent=4)
 
-# Print the JSON data
-print(json_data)
+# Write the JSON data to the prompt_dictionary.json file
+with open('prompt_dictionary.json', 'w') as json_file:
+  json_file.write(json_data)
